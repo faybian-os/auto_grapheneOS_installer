3cbde0a2b0f534aea7488ebec06817a689683aff
whimsy <whimsy@sylph.box>
2025-03-26 16:28:04 -0500

    apt

--- app_sh/get-apt.sh
+++ app_sh/get-apt.sh
@@ -2,5 +2,4 @@
 # File_Rel_Path: 'app_sh/get-apt.sh'
 
 ./app_sh/echo_header.sh --no_leading_whitespace "Installing needed apt packages."
-sudo apt install libarchive-tools android-sdk-platform-tools-common openssh-client signify-openbsd jq curl
-
+sudo apt-get install libarchive-tools android-sdk-platform-tools-common openssh-client signify-openbsd jq curl -y
